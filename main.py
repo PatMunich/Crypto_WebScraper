492cb05c9a13f42afb60716efa024bfac09cd0d9
PatrickMacBook <justadd7@gmx.de>
2025-04-26 00:59:52 +0200

    embed TelegramManager

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 from WebScraper import WebScraper
 from WorkbookEditor import WorkbookEditor
+from TelegramManager import TelegramManager
 
 # Defines
-target_website = 'https://backprop.finance/dtao/profile/5Cd5nSe1PzuGteZ3vSCZs8pcHxqy3wwmYcpHznK5Fbctu27W'
+TARGET_WEBSITE = 'https://backprop.finance/dtao/profile/5Cd5nSe1PzuGteZ3vSCZs8pcHxqy3wwmYcpHznK5Fbctu27W'
+USER = 'Patrick'
 
 if __name__ == '__main__':
-    WebScraper = WebScraper(target_website)
+    WebScraper = WebScraper(TARGET_WEBSITE)
     WorkbookEditor = WorkbookEditor()
+    TelegramManager = TelegramManager(USER)
     while True:
         WebScraper.getTimestamp()
         if WebScraper.performanceTimerWebsiteLoading():
@@ -20,6 +23,10 @@ if __name__ == '__main__':
             print(f"[WebScraper status] {WebScraper.timestamp} result: "
                   f"{str(dict(list(WebScraper.result_dict.items())))}")
             WorkbookEditor.sortInSubnetData(WebScraper.provideResultData())
+            if TelegramManager.sendMessage(WebScraper.provideResultData()):
+                pass
+            else:
+                print("[TelegramManager error] while trying to send daily rewards update message!")
         else:
-            print(f"[WebScraper error] trying to execute web-scraper {str(WebScraper.response.status_code)}")
+            print(f"[WebScraper error] trying to execute web-scraper {str(WebScraper.response.status_code)}!")
         time.sleep(86400)
